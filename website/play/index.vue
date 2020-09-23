eb0a66528440ad39efd58615281ad090fea3621c
JeremyWuuuuu <591449570@qq.com>
2020-09-24 00:02:25 +0800

    feat(popover): component almost done. next `directive`

--- website/play/index.vue
+++ website/play/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-  </div>
+  <el-popover content="test content" title="标题">
+    <template #reference>
+      <el-button>I am reference</el-button>
+    </template>
+  </el-popover>
 </template>
 
 <script>
